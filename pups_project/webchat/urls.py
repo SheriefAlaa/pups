3fa994c81cca096b914306b6102e0b4e648920c9
Sherief Alaa <sheriefalaa.w@gmail.com>
2014-04-13 17:25:28 +0200

    removed a useless urlpattern

--- pups_project/webchat/urls.py
+++ pups_project/webchat/urls.py
@@ -4,6 +4,5 @@ urlpatterns = patterns('',
     url(r'^$', 'webchat.views.home'), # Root site, explains to users how to use webchat.
     url(r'^chat/(?P<token>\w{0,32})$', 'webchat.views.chat'), # chat with a support assistant
     url(r'^chpass$', 'webchat.views.change_password'), 
-    url(r'^logged$', 'webchat.views.logged_in'),
     url(r'^tokens$', 'webchat.views.tokens_page'), # Token management page
 )
