c93e4ac9ba149d8702fd8b0cb59742856a66f61e
Sherief Alaa <sheriefalaa.w@gmail.com>
2014-04-13 18:08:17 +0200

    changed the control flow of change_password()

--- pups_project/webchat/forms.py
+++ pups_project/webchat/forms.py
@@ -10,11 +10,9 @@ class ChangePassForm(forms.Form):
     new_pass_confirm = forms.CharField(max_length=32, widget=forms.PasswordInput)
 
     def change_password(self, request, data):
-        if (data['new_pass'] == data['new_pass_confirm'] ) and \
-            request.user.check_password(data['current_pass'] ):
-
+        if not ( data['new_pass'] == data['new_pass_confirm'] and request.user.check_password(data['current_pass']) ):
+            return False
+        else:
             request.user.set_password(data['new_pass'])
             request.user.save()
             return True
-        else:
-            return False
