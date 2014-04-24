b1cac0f531114aad8894c44ab34eb199f4541b4b
Sherief Alaa <sheriefalaa.w@gmail.com>
2014-04-24 17:22:48 +0200

    removed pups_project cached files

--- pups_project/pups/templatetags/base_extras.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.simple_tag
-def navactive(request, urls):
-    if urls in request.path:
-        return "active"
-    return ""
--- pups_project/stats/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
-
-
-def stats(request):
-
-    return render (request, 'stats.html')
