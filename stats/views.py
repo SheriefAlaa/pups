d4813decec3314077087eae43344d26caba6d507
Sherief Alaa <sheriefalaa.w@gmail.com>
2014-05-18 01:36:48 +0300

    /stats now requires login

--- stats/views.py
+++ stats/views.py
@@ -22,8 +22,10 @@
 
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def stats(request):
 
     return render(request, 'stats.html')
